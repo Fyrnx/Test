b77b2647abf933ba05b3ef3d0fb469c0484e47c0
AhmedBaghododa <125276501+Fyrnx@users.noreply.github.com>
2023-12-18 00:35:31 +0200

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const port = process.env.PORT ?? 6000
 let puppeteer = require("puppeteer").default
+let http = require("http")
+let url = require("url");
 
+let browser = 
 puppeteer.launch({
     headless: "new"
+}).then(result => { 
+    browser = result
 })
 
-console.log("end")
+http.createServer((req,res) => {
+    console.log(browser);
+    res.end(req.url);
+}).listen(port,_ => { 
+    console.log(`listing to ${port}`)
+})
