fcd7589e437ece0922a7a5020cb4fa32ee41fd0b
AhmedBaghododa <125276501+Fyrnx@users.noreply.github.com>
2023-12-18 02:10:25 +0200

    test1

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const port = process.env.PORT ?? 6000
 let puppeteer = require("puppeteer").default
 let http = require("http")
 let url = require("url");
+let browser 
 
-let browser = 
 puppeteer.launch({
     headless: "new"
 }).then(result => { 
     browser = result
 })
 
-http.createServer((req,res) => {
-    console.log(browser);
-    res.end(req.url);
+http.createServer(async (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, PATCH, DELETE');
+
+
+    
+    if(browser == undefined) return res.end()
+    let page = await browser.newPage()
+
+    try {
+        await page.goto(url, {timeout: 0});
+    } catch(err) {}
+
+    let imageSrc = await page.evaluate(_ => { 
+        let a = document.querySelector(".mw-wiki-logo")
+        return getComputedStyle(a).backgroundImage.match(/http[^"']+/ig)[0]
+    })
+
+    console.log(imageSrc);
+    res.end(imageSrc)
 }).listen(port,_ => { 
     console.log(`listing to ${port}`)
 })
